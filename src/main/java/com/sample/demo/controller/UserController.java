448f110f7cfb6f020af3f62de6abb0f39f8241f4
it21156960 <chamith227@gmail.com>
2023-06-22 15:25:01 +0530

    Initial commit

---
+++ src/main/java/com/sample/demo/controller/UserController.java
@@ -0,0 +1,32 @@
+package com.sample.demo.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value= "api/v1/user")
+@CrossOrigin
+
+public class UserController {
+
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "Hehe";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(){
+        return "User saved";
+    }
+
+    @PutMapping("/updateUser")
+    public String updateUser(){
+        return "User updated";
+    }
+
+    @DeleteMapping ("/deleteUser")
+    public String deleteUser(){
+        return "User deleted";
+    }
+
+
+}
